c59c9398e734314afbc78f2fc8f39c20e96b7cd7
Reem Elgarhy <113386619+ReemElgarhy@users.noreply.github.com>
2024-01-31 04:19:40 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+amount= int(input(" what is the amount in bank that has been there for more than a year ?"))
+if amount>2000 :
+ print(0.025*amount , " this is the zakat you have to pay ")
+  
+else :
+ print(" you dont have to pay zakat")
